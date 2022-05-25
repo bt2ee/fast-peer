69411eb2a6adcae993e7beca4a3a1df65d706bf1
chenle <chenle@forchange.tech>
2022-05-25 22:27:39 +0800

    feat: init peerjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export const foo: 'foo' = 'foo';
+import Peer, { DataConnection } from 'peerjs'
+
+function CreatePeer(id: string){
+  const peer = new Peer()
+
+  peer.on('connection', (connection: DataConnection) => {
+    connection.on('data', data => {
+      console.log(data)
+    })
+  })
+
+  const connector = peer.connect(id)
+  connector.on('open', () => {
+    console.log(open)
+  })
+}
+
+export default CreatePeer
+
+// todo list
+// 数据传递
+// 多媒体输出
+// 断开连接
+// 获取错误重试机制
+// 心跳机制
